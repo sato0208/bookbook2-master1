4d1df714d9aa96aede5dcbf84565d5cd18ae70b3
sato chiharu <kujira028@gmail.com>
2020-04-07 09:03:15 +0000

    課題8

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,8 +1,6 @@
 class UserMailer < ApplicationMailer
-	default from:'kujira028@gmail.com'
-  
 	def user_welcome_mail(user)
 	    @user = user
-		mail(to: @user.email, subject: 'Welcome to Bookers!yay!')
+		mail(to: user.email, subject: 'Welcome to Bookers!yay!')
 	end
 end
