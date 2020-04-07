43c18ad172f9fe08c69ecb6a0169be79846d60b7
sato chiharu <kujira028@gmail.com>
2020-04-07 09:46:48 +0000

    Revert "課題8"
    This reverts commit 4d1df714d9aa96aede5dcbf84565d5cd18ae70b3.

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,6 +1,8 @@
 class UserMailer < ApplicationMailer
+	default from:'kujira028@gmail.com'
+  
 	def user_welcome_mail(user)
 	    @user = user
-		mail(to: user.email, subject: 'Welcome to Bookers!yay!')
+		mail(to: @user.email, subject: 'Welcome to Bookers!yay!')
 	end
 end
